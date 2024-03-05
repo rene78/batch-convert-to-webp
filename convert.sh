0340e187de4894c444c6aa6e74b6594e92971456
rene78 <36115705+rene78@users.noreply.github.com>
2024-03-05 17:39:47 +0100

    Match filenames in a case-insensitive fashion, e.g. ".jpg" and ".JPG" both work. Closes #1

--- convert.sh
+++ convert.sh
@@ -1,7 +1,8 @@
 mkdir "webp" #Create a subfolder named "webp"
 shopt -s nullglob #ignore a failed match and do not enter the body of the loop, e.g. if there was no tiff files in folder ./cwebp would still be called with *.tiff. We prevent that with nullglob (https://stackoverflow.com/a/14505622/5263954)
+shopt -s nocaseglob #matches filenames in a case-insensitive fashion, e.g. ".jpg" and ".JPG" both work
 for file in *.{png,jpg,jpeg,tiff,tif} #Loop through all image files in folder.
 do
   ./cwebp -q 80 "$file" -o "webp/${file%.*}.webp" #Create webp image file with quality level 80 and save it into the wepb subfolder
-  # echo "${file}" #Just some bugfixing code. Can be ignored.
+  echo "${file}" #Just some bugfixing code. Can be ignored.
 done
